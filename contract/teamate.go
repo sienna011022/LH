0332ca5a86398256c245cb182c8bf42933066ca0
sienna <202000803@sungshin.ac.kr>
2022-04-07 11:37:32 +0900

    chaincode

--- contract/teamate.go
+++ contract/teamate.go
@@ -8,6 +8,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"strconv"
+	"time"
+
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
@@ -15,216 +17,87 @@ import (
 type SmartContract struct {
 	contractapi.Contract
 }
-type UserRating struct{
-	User string `json:"user"`
-	Average float64 `json:"average"`
-	Rates []Rate `json:"rates"`
 
+type RightProcess struct {
+	key       string     `json:"key"`
+	name      string     `json:"name"`
+	state     string     `json:"state"`
+	Contracts []Contract `json:"Contracts"`
 }
 
-type Rate struct{
-	projectTitle string `json:"projecttitle"`
-	Score float64 `json:"score"`
+type Contract struct {
+	docu_id       uint64    `json:"docu_id"`
+	docu_name     string    `json:"docu_name"`
+	document_hash string    `json:"document_hash"`
+	timestamp     time.Time `json:"timestamp"`
 }
 
 //fuction
+func (s *SmartContract) AddUser(ctx contractapi.TransactionContextInterface, key string) error {
 
-func (s *SmartContract)AddUser(ctx contractapi.TransactionContextInterface,username string) error {
-	//error check
-
-
-
-	
 	//marshal
-	var user = UserRating{User:username,Average : 0 }
-	userAsBytes,_ := json.Marshal(user)
-	return  ctx.GetStub().PutState(username,userAsBytes)
-
-
-}
-func (s *SmartContract)AddRating(ctx contractapi.TransactionContextInterface,username string,prjTitle string,prjscore string) error {
-	
-  userAsBytes,err:= ctx.GetStub().GetState(username)
-
-
-  if err != nil{
-	  return err
-  }else if userAsBytes == nil{
-	  return fmt.Errorf("User does not exist "+username+"/")
-  }
-
-  user  := UserRating{}
-  err= json.Unmarshal(userAsBytes,&user)
-  if err != nil{
-	  return err
-  }
-
-  newRate ,_ := strconv.ParseFloat(prjscore,64)
-  var Rate = Rate{projectTitle:prjTitle,Score : newRate}
-
-  rateCount := float64(len(user.Rates))
-
-  user.Rates= append(user.Rates,Rate)
-
-  user.Average = (rateCount * user.Average + newRate)/(rateCount+1)
-  userAsBytes,err = json.Marshal(user);
-
-  if err !=nil{
-	  return fmt.Errorf("failed to Marshaling:%v",err)
-
-  }
-
-  err = ctx.GetStub().PutState(username,userAsBytes)
-
-  if err != nil{
-	  return fmt.Errorf("failed to AddRating %v",err)
-
-  }
-
-  return nil
-
-
-
-
-}
-func (s *SmartContract)ReadRating(ctx contractapi.TransactionContextInterface,username string) (string,error) {
-	//get value from ctx
-	userAsBytes, err := ctx.GetStub().GetState(username)
-
-	if(err != nil){
-		return "",fmt.Errorf("failed to read from world state,%s", err.Error())
-	}
-	
-	if (userAsBytes == nil) {
-		return "",fmt.Errorf("%s  does not exist",username)
-
-	}
-
-	return string(userAsBytes[:]),nil
-
-
-
-
+	var request = RightProcess{key: key}
+	requestAsBytes, _ := json.Marshal(request)
+	return ctx.GetStub().PutState(key, requestAsBytes)
 
 }
 
-// Car describes basic details of what makes up a car
-type Car struct {
-	Make   string `json:"make"`
-	Model  string `json:"model"`
-	Colour string `json:"colour"`
-	Owner  string `json:"owner"`
-}
-
-// QueryResult structure used for handling result of query
-type QueryResult struct {
-	Key    string `json:"Key"`
-	Record *Car
-}
-
-// InitLedger adds a base set of cars to the ledger
-func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
-	cars := []Car{
-		Car{Make: "Toyota", Model: "Prius", Colour: "blue", Owner: "Tomoko"},
-		Car{Make: "Ford", Model: "Mustang", Colour: "red", Owner: "Brad"},
-		Car{Make: "Hyundai", Model: "Tucson", Colour: "green", Owner: "Jin Soo"},
-		Car{Make: "Volkswagen", Model: "Passat", Colour: "yellow", Owner: "Max"},
-		Car{Make: "Tesla", Model: "S", Colour: "black", Owner: "Adriana"},
-		Car{Make: "Peugeot", Model: "205", Colour: "purple", Owner: "Michel"},
-		Car{Make: "Chery", Model: "S22L", Colour: "white", Owner: "Aarav"},
-		Car{Make: "Fiat", Model: "Punto", Colour: "violet", Owner: "Pari"},
-		Car{Make: "Tata", Model: "Nano", Colour: "indigo", Owner: "Valeria"},
-		Car{Make: "Holden", Model: "Barina", Colour: "brown", Owner: "Shotaro"},
-	}
-
-	for i, car := range cars {
-		carAsBytes, _ := json.Marshal(car)
-		err := ctx.GetStub().PutState("CAR"+strconv.Itoa(i), carAsBytes)
-
-		if err != nil {
-			return fmt.Errorf("Failed to put to world state. %s", err.Error())
-		}
-	}
-
-	return nil
-}
-
-// CreateCar adds a new car to the world state with given details
-func (s *SmartContract) CreateCar(ctx contractapi.TransactionContextInterface, carNumber string, make string, model string, colour string, owner string) error {
-	car := Car{
-		Make:   make,
-		Model:  model,
-		Colour: colour,
-		Owner:  owner,
-	}
-
-	carAsBytes, _ := json.Marshal(car)
-
-	return ctx.GetStub().PutState(carNumber, carAsBytes)
-}
-
-// QueryCar returns the car stored in the world state with given id
-func (s *SmartContract) QueryCar(ctx contractapi.TransactionContextInterface, carNumber string) (*Car, error) {
-	carAsBytes, err := ctx.GetStub().GetState(carNumber)
+func (s *SmartContract) AddContract(ctx contractapi.TransactionContextInterface, key string, name string, state string, docu_id string, docu_name string, document_hash string) error {
+	requestAsBytes, err := ctx.GetStub().GetState(key)
 
 	if err != nil {
-		return nil, fmt.Errorf("Failed to read from world state. %s", err.Error())
+		return err
+	} else if requestAsBytes == nil {
+		return fmt.Errorf("User does not exist " + key + "/")
 	}
 
-	if carAsBytes == nil {
-		return nil, fmt.Errorf("%s does not exist", carNumber)
-	}
-
-	car := new(Car)
-	_ = json.Unmarshal(carAsBytes, car)
-
-	return car, nil
-}
-
-// QueryAllCars returns all cars found in world state
-func (s *SmartContract) QueryAllCars(ctx contractapi.TransactionContextInterface) ([]QueryResult, error) {
-	startKey := ""
-	endKey := ""
-
-	resultsIterator, err := ctx.GetStub().GetStateByRange(startKey, endKey)
-
-	if err != nil {
-		return nil, err
-	}
-	defer resultsIterator.Close()
-
-	results := []QueryResult{}
-
-	for resultsIterator.HasNext() {
-		queryResponse, err := resultsIterator.Next()
-
-		if err != nil {
-			return nil, err
-		}
-
-		car := new(Car)
-		_ = json.Unmarshal(queryResponse.Value, car)
-
-		queryResult := QueryResult{Key: queryResponse.Key, Record: car}
-		results = append(results, queryResult)
-	}
-
-	return results, nil
-}
-
-// ChangeCarOwner updates the owner field of car with given id in world state
-func (s *SmartContract) ChangeCarOwner(ctx contractapi.TransactionContextInterface, carNumber string, newOwner string) error {
-	car, err := s.QueryCar(ctx, carNumber)
+	request := RightProcess{}
+	err = json.Unmarshal(requestAsBytes, &request)
 
 	if err != nil {
 		return err
 	}
 
-	car.Owner = newOwner
+	time := time.Now()
 
-	carAsBytes, _ := json.Marshal(car)
+	docu_id64, _ := strconv.ParseInt(docu_id, 10, 64)
 
-	return ctx.GetStub().PutState(carNumber, carAsBytes)
+	Contract := Contract{docu_id: uint64(docu_id64), docu_name: docu_name, document_hash: document_hash, timestamp: time}
+
+	request.Contracts = append(request.Contracts, Contract)
+
+	requestAsBytes, err = json.Marshal(request)
+
+	if err != nil {
+		return fmt.Errorf("failed to Marshaling:%v", err)
+
+	}
+
+	err = ctx.GetStub().PutState(key, requestAsBytes)
+
+	if err != nil {
+		return fmt.Errorf("failed to AddContract %v", err)
+
+	}
+
+	return nil
+
+}
+func (s *SmartContract) ReadContract(ctx contractapi.TransactionContextInterface, key string) (string, error) {
+	//get value from ctx
+	contractAsBytes, err := ctx.GetStub().GetState(key)
+
+	if err != nil {
+		return "", fmt.Errorf("failed to read from world state,%s", err.Error())
+	}
+
+	if contractAsBytes == nil {
+		return "", fmt.Errorf("%s  does not exist", key)
+
+	}
+
+	return string(contractAsBytes[:]), nil
+
 }
 
 func main() {
